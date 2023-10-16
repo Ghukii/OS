601694cf100c5defe72d42fd56de3844aa94f242
Ghukii <pringles4823@gmail.com>
2023-10-16 17:21:30 +0300

    t2 t3 izm

--- LABA_2/task_3.py
+++ LABA_2/task_3.py
@@ -2,10 +2,8 @@ import threading
 import time
 import psutil
 
-# Глобальная переменная для хранения состояния потоков
 threads_paused = False
 
-# Функция для выполнения работы в потоках
 def worker_thread(thread_id):
     while True:
         if not threads_paused:
@@ -15,20 +13,17 @@ def worker_thread(thread_id):
             print(f"Поток {thread_id} приостановлен")
             time.sleep(1)
 
-# Функция для отслеживания загрузки процессора
 def monitor_cpu_usage():
     while True:
         cpu_percent = psutil.cpu_percent(interval=1)
         print(f"Загрузка процессора: {cpu_percent}%")
 
-# Создаем и запускаем потоки
 threads = []
 for i in range(2):
     thread = threading.Thread(target=worker_thread, args=(i,))
     thread.start()
     threads.append(thread)
 
-# Создаем и запускаем поток для мониторинга загрузки процессора
 cpu_monitor_thread = threading.Thread(target=monitor_cpu_usage)
 cpu_monitor_thread.start()
 
@@ -40,7 +35,7 @@ while True:
     elif user_input == 'возобновить':
         threads_paused = False
     elif user_input == 'выход':
-        # Завершаем все потоки
+
         for thread in threads:
             thread.join()
         cpu_monitor_thread.join()
